700a62b1d9e7c6e4604f873516aa2acb4e65f74b
Kolawole Emmanuel Teke <kolawoleteke96@gmail.com>
2025-07-07 20:55:40 +0100

    made changes to the vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-  },
-  base: "TRAVEL_WEBSITE",
+  // server: {
+  //   host: true,
+  // },
+  base: "/TRAVEL_WEBSITE",
 });
